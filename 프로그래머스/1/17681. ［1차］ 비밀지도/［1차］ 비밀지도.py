465460c747f35bdc9f279ea102ec5f6fc008127d
Yujin Kim <126761511+eidus@users.noreply.github.com>
2024-06-13 23:37:21 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,22 @@
+def make_binary(n, num):
+    miro = ['#']*n
+    for i in range(n-1, -1, -1):
+        if num%2 != 1:
+            miro[i]= ' '
+        num = num//2
+    return miro
+
+def solution(n, arr1, arr2):
+    answer = []
+    
+    for i, j in zip(arr1, arr2):
+        a = make_binary(n, i)
+        b = make_binary(n, j)
+        row_list = ''
+        for k, p in zip(a, b):
+            if k == '#' or p == '#': row_list += '#'
+            else: row_list += ' '
+        answer.append(row_list)
+    print(answer)
+    
+    return answer
