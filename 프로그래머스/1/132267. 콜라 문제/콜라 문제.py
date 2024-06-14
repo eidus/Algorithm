579f41c0ac5951c597d95a3576982d5ff6741658
Yujin Kim <126761511+eidus@users.noreply.github.com>
2024-06-14 21:17:43 +0900

    [level 1] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -0,0 +1,10 @@
+def solution(a, b, n):
+    answer = 0
+    remain = 0
+    while n>=a:
+        remain = n%a
+        n = (n//a)*b
+        answer += n
+        n += remain
+    
+    return answer
